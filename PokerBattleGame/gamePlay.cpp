a6213d04a255b97fde5ce4ee6af3a973966e2710
seong min Kim <skattr13@naver.com>
2019-09-21 01:22:28 +0900

    Implement card shuffle

--- PokerBattleGame/gamePlay.cpp
+++ PokerBattleGame/gamePlay.cpp
@@ -5,12 +5,27 @@
 
 using namespace std;
 
+void cardShuffle();
+
 string card[40]
 { "¢¼1", "¢¼2", "¢¼3", "¢¼4", "¢¼5", "¢¼6", "¢¼7", "¢¼8", "¢¼9", "¢¼10",
 	"¡ß1", "¡ß2", "¡ß3", "¡ß4", "¡ß5", "¡ß6", "¡ß7", "¡ß8", "¡ß9", "¡ß10",
 	"¢¾1", "¢¾2", "¢¾3", "¢¾4", "¢¾5", "¢¾6", "¢¾7", "¢¾8", "¢¾9", "¢¾10",
 	"¢À1", "¢À2", "¢À3", "¢À4", "¢À5", "¢À6", "¢À7", "¢À8", "¢À9", "¢À10" };
 
+vector <int> cardNumber;
 void gamePlay() {
 	srand((unsigned int)time(0));
+
+	cardShuffle();
+}
+
+void cardShuffle() {
+	int number = rand() % 40;
+	for (int cardCounter = 0; cardCounter < 40; cardCounter++) {
+		while (count(cardNumber.begin(), cardNumber.end(), number)) {
+			number = rand() % 40;
+		}
+		cardNumber.push_back(number);
+	}
 }
